ac702ea9897ad314d523d77da86637d9f12ea1b4
Katherin Cuautli <kcuautli934@gmail.com>
2026-06-28 15:37:27 -0400

    Added gallery and footer to home and improved stling to sretch across page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -65,6 +65,43 @@ function Home(){
                 </div>
             </div>
         </section>
+
+        <section className="gallery">
+            <h2>Gallery</h2>
+            <div className="slider-container">
+                <button id="prevB">❮</button>
+                <img id="sliderImage"
+                     src="images/enchiladas.jpg"
+                     alt="Gallery Image"/>
+                <button id="nextB">❯</button>
+            </div>
+        </section>
+        <script src="script.js"></script>
+
+        <footer>
+            <div className="footer-content">
+                <div className="business-hours">
+                    <h3>Business Hours</h3>
+                    <p>Monday - Thursday: 11 AM - 9 PM</p>
+                    <p>Friday - Sunday: 11 AM - 11 PM</p>
+                </div>
+        
+                <div className="social-links">
+                    <h3>Follow Us</h3>
+                    <a href="https://www.facebook.com" target="_blank">
+                        Facebook
+                    </a>
+        
+                    <a href="https://www.instagram.com" target="_blank">
+                        Instagram
+                    </a>
+        
+                    <a href="https://www.tiktok.com" target="_blank">
+                        TikTok
+                    </a>
+                </div>
+            </div>
+        </footer>
         </>
     );
 }
