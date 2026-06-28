905b0844dcf2d461176f7a56a768d90cbbe53a37
Katherin Cuautli <kcuautli934@gmail.com>
2026-06-28 17:45:19 -0400

    Finished rendering menu page and added footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,30 @@
+function Footer() {
+    return (
+        <footer>
+            <div class="footer-content">
+                <div class="business-hours">
+                    <h3>Business Hours</h3>
+                    <p>Monday - Thursday: 11 AM - 9 PM</p>
+                    <p>Friday - Sunday: 11 AM - 11 PM</p>
+                </div>
+        
+                <div class="social-links">
+                    <h3>Follow Us</h3>
+                    <a href="https://www.facebook.com" target="_blank" rel="noreferrer">
+                        Facebook
+                    </a>
+        
+                    <a href="https://www.instagram.com" target="_blank" rel="noreferrer">
+                        Instagram
+                    </a>
+        
+                    <a href="https://www.tiktok.com" target="_blank" rel="noreferrer">
+                        TikTok
+                    </a>
+                </div>
+            </div>
+        </footer>
+    );
+}
+  
+export default Footer;
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,5 +1,88 @@
+import enchiladas from "../assets/enchiladas.jpg";
+import tacos from "../assets/tacos.jpg";
+import quesadillas from "../assets/quesadillas.jpg";
+import tostadas from "../assets/tostadas.jpg";
+import nachos from "../assets/nachos.jpg";
+import torta from "../assets/torta.jpg";
+
 function Menu() {
-  return <h1>Menu Page</h1>;
+  return(
+    <>
+      <section className="menu-selection">
+          <h1>Our Menu</h1>
+          <div className="menu-container">
+              <div className="menu-card">
+                  <img src={enchiladas} alt="Enchiladas" />
+                  <h3>Enchiladas</h3>
+                  <p>$13.99</p>
+                  <button>
+                      Add To Cart
+                  </button>
+              </div>
+
+              <div className="menu-card">
+                  <img src={tacos} alt="Tacos" />
+                  <h3>Tacos al Pastor</h3>
+                  <p>$6.99</p>
+                  <button>
+                      Add To Cart
+                  </button>
+              </div>
+
+              <div className="menu-card">
+                  <img src={quesadillas} alt="Quesadillas" />
+                  <h3>Quesadillas</h3>
+                  <p>$8.99</p>
+                  <button>
+                      Add To Cart
+                  </button>
+              </div>
+
+              <div className="menu-card">
+                  <img src={tostadas} alt="Tostadas" />
+                  <h3>Tostadas</h3>
+                  <p>$10.99</p>
+                  <button>
+                      Add To Cart
+                  </button>
+              </div>
+
+              <div className="menu-card">
+                  <img src={nachos} alt="Nachos" />
+                  <h3>Nachos</h3>
+                  <p>$7.99</p>
+                  <button>
+                      Add To Cart
+                  </button>
+              </div>
+
+              <div className="menu-card">
+                  <img src={torta} alt="Torta" />
+                  <h3>Torta</h3>
+                  <p>$15.99</p>
+                  <button>
+                      Add To Cart
+                  </button>
+              </div>
+          </div>
+      </section>
+
+      <section className="cart-section">
+          <h2>Shopping Cart</h2>
+          <ul id="cartItems">
+          </ul>
+          <h3>
+              Total: $
+              <span id="totalPrice">
+                  0.00
+              </span>
+          </h3>
+          <button>
+              Clear Cart
+          </button>
+      </section>
+    </>
+  )
 }
 
 export default Menu;
