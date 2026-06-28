6497c876fca2aafb7149d7af852eb6c626fe8123
Katherin Cuautli <kcuautli934@gmail.com>
2026-06-28 18:04:12 -0400

    Finished About react page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,36 @@
-function About() {
-  return <h1>About Page</h1>;
+function About(){
+  return (
+    <>
+    <section className="about">
+      <h1>About Us</h1>
+      <h4>Our Mission</h4>
+      <p>
+        We strive to ensure authentic Mexican flavors in every dish through our
+        use of fresh ingredients and homemade recipes. 
+      </p>
+
+      <h4>History: How We Got Started</h4>
+      <p>
+        Our restaurant began as a family dream in the small town of
+        Huejotzingo, Mexico. Doña Maria, owner of the house and land,
+        started off by cooking just for her beuatiful children and 
+        husband. It was her favorite pass time as she enjoyed spreading
+        joy to the people around her. She kept all her favorite recipes 
+        in her recipe book which was later used by her children when they
+        grew old enough to learn. Her daughter Esmeralda thought her 
+        cooking and recipes were something special, so she decided to 
+        share it with the world. At first, it was just the neighbors, but 
+        soon after her and her brothers got the liscence to sell their 
+        food in a food truck. Not long after, a small restaurant was 
+        opened in the main city of Mexico. It got so popular that multiple
+        restaurants started opening up around Mexico. A couple years after, 
+        these amazing recipes were brought to America and multiple restaurants
+        opened up there as well. We hope to continue expanding and bringing
+        the flavor of Mexico to everyone around the world! 
+      </p>
+    </section>
+    </>
+  );
 }
   
 export default About;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,32 +19,6 @@ function Home(){
                 <button id="nextB">❯</button>
             </div>
         </section>
-        <script src="script.js"></script>
-
-        <footer>
-            <div className="footer-content">
-                <div className="business-hours">
-                    <h3>Business Hours</h3>
-                    <p>Monday - Thursday: 11 AM - 9 PM</p>
-                    <p>Friday - Sunday: 11 AM - 11 PM</p>
-                </div>
-        
-                <div className="social-links">
-                    <h3>Follow Us</h3>
-                    <a href="https://www.facebook.com" target="_blank">
-                        Facebook
-                    </a>
-        
-                    <a href="https://www.instagram.com" target="_blank">
-                        Instagram
-                    </a>
-        
-                    <a href="https://www.tiktok.com" target="_blank">
-                        TikTok
-                    </a>
-                </div>
-            </div>
-        </footer>
         </>
     );
 }
